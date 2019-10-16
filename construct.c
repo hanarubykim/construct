35af426b595daade889d6978753814a5a0036ed1
Hana Kim <hkim00@stuy.edu>
2019-10-16 13:20:37 -0400

    ddd

---
+++ construct.c
@@ -0,0 +1,25 @@
+// Create a struct that has at least 2 data members, one must be a string.
+//  You can provide struct prototypes outside of functions.
+// Don't just combine random things, make it meaningful (like food and price, athlete and associated statistic, Pokemon and something about Pokemon ..)
+// Write a function that returns an example of your struct when run.
+//     This should not return the same values every time.
+// Write a function that prints out variables of your structs type in a reasonable way.
+// Write a function that modifies values of your struct's type.
+// Write a main function that tests all these things.
+//     Have meaningful output.
+// DONT FORGET A MAKEFILE
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct brunch {char[] str; int ranking;};
+
+int main(){
+  struct brunch waffles;
+  waffles.str
+
+
+
+
+
+}
