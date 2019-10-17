7b4efe2b875034397d9c9aa348fb60734f4ecf46
hanarubykim <hanarubykim2002@gmail.com>
2019-10-16 21:18:27 -0400

    updated

--- construct.c
+++ construct.c
@@ -1,36 +1,47 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 struct brunch brunchItem;
 
 struct brunch{
-  char *name;
+  char* name;
   int price;
 };
 
-char *menu[] = {"waffles", "pancakes", "avocado toast", "croissant", "omelets"};
+char* menu[] = {"waffles", "pancakes", "avocado toast", "croissant", "omelets"};
 int prices[] = {3, 5, 13, 12, 1, 9};
 
 struct brunch randomMenuItem(){
-  brunch item;
-  int decideFood = (rand() * 6) / 100;
-  item.name = *menu[decideFood];
+  srand(time(NULL));
+  struct brunch item;
+  int decideFood = rand() % 6;
+  item.name = menu[decideFood - 1];
 
-  int decidePrice = (rand() * 6) / 100;
+  int decidePrice = rand() % 6;
   item.price = prices[decidePrice];
 
   return item;
 }
 
-void printBrunchItem(brunch item){
-  printf("Brunch time! Your brunch food will be... %c and costs $%d", item.name, item.price);
+void printBrunchItem(struct brunch item){
+  printf("Brunch time! Your brunch food will be... %c and costs $%d\n\n", item.name, item.price);
 }
 
-brunch changeTo(char food, int price){
-  brunch item;
-  item.name = food;
+struct brunch changeTo(char *food, int price){
+  struct brunch item;
+  item.name = *food;
   item.price = price;
 
   return item;
+}
+
+int main(){
+  struct brunch item1;
+  item1 = randomMenuItem();
+  printBrunchItem(item1);
+
+  item1 = changeTo("bagel", 2);
+  printBrunchItem(item1);
 }
